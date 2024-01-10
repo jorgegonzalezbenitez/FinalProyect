695e99b0c1c39be8d28895087a7d0b77f91bd9b1
first commit <jorgeglezbenitez@gmail.com>
2024-01-10 20:16:39 +0000

    Project Corrected

--- prediction-provider/src/main/java/org/example/control/WeatherProvider.java
+++ prediction-provider/src/main/java/org/example/control/WeatherProvider.java
@@ -2,7 +2,6 @@ package org.example.control;
 import org.example.model.*;
 import java.time.Instant;
 
-
 public interface WeatherProvider {
     Weather getWeather(Location location,Instant instant);
 }
